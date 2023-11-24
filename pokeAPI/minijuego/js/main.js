93d5f9c6bd43310023cc7bdb1940d180727c64db
Avilsan04 <145869111+Avilsan04@users.noreply.github.com>
2023-11-24 12:46:52 +0100

    Add files via upload

---
+++ pokeAPI/minijuego/js/main.js
@@ -0,0 +1,27 @@
+// Get aleatory pokémon
+const getPokemon = () => {
+    let randomId = Math.floor(Math.random() * 150 + 1);
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://pokeapi.co/api/v2/pokemon/' + randomId
+    }).done(res => {
+        
+        const pokemon = {
+            id: res.id,
+            name: res.species.name,
+            img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${ res.id }.png`
+        };
+
+        console.log(pokemon);
+        document.getElementById("pokemon-image").src = pokemon.img;
+        document.getElementById("pokemon-image").title = pokemon.name;
+    });
+};
+
+getPokemon();
+
+let refresh = document.getElementById('refresh');
+refresh.addEventListener('click', _ => {
+            location.reload();
+})
